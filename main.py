c3b752cb35f9be7979be42bfbdb39d26fcd8f511
harshjain56282-lab <harshjain56282@gmail.com>
2025-08-10 17:00:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random, asyncio
+import random
 
 def main():
    num_generator = random.randrange(1, 100)
@@ -6,5 +6,5 @@ def main():
    num = int(input("Enter number: "))
    # if num == num_generator:
    print("Right number!" if num == num_generator else "Wrong number!")
-   await asyncio.sleep(0)
-asyncio.run(main())
+
+main()
